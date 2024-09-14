c5223adb969b2ad46ebf8af86c504c96c7405f42
Sprite_Spirit <Sprite.spirit.t.me@gmail.com>
2024-09-14 20:49:54 +0300

    Создан класс сотрудника, 2 конструктора, геттеры и сеттеры

---
+++ src/Employee.java
@@ -0,0 +1,196 @@
+/**
+ * Класс, представляющий сотрудника в организации.
+ * Содержит информацию об имени, фамилии, отчестве, отделе и зарплате сотрудника.
+ */
+
+public class Employee {
+    /**
+     * Имя сотрудника
+     */
+    private String first_name;
+    /**
+     * Фамилия сотрудника
+     */
+    private String last_name;
+    /**
+     * Отчество сотрудника
+     */
+    private String middle_name;
+    /**
+     * Номер отдела, в котором работает сотрудник
+     */
+    private int department;
+    /**
+     * Зарплата сотрудника
+     */
+    private float salary;
+    /**
+     * Уникальный идентификатор сотрудника
+     */
+    private static int id = 1;
+
+    /**
+     * Конструктор с отчеством.
+     *
+     * @param first_name  Имя сотрудника
+     * @param last_name   Фамилия сотрудника
+     * @param middle_name Отчество сотрудника
+     * @param department  Отдел сотрудника
+     * @param salary      Зарплата сотрудника
+     */
+
+    public Employee(String first_name, String last_name, String middle_name, int department, float salary) {
+        setFirst_name(first_name);
+        setLast_name(last_name);
+        setMiddle_name(middle_name);
+        setDepartment(department);
+        setSalary(salary);
+        this.id = id++;
+    }
+
+    /**
+     * Конструктор без отчества.
+     *
+     * @param first_name Имя сотрудника
+     * @param last_name  Фамилия сотрудника
+     * @param department Отдел сотрудника
+     * @param salary     Зарплата сотрудника
+     */
+
+    public Employee(String first_name, String last_name, int department, float salary) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.department = department;
+        this.salary = salary;
+        this.id = id++;
+    }
+
+    /**
+     * Возвращает имя сотрудника.
+     *
+     * @return имя сотрудника
+     */
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    /**
+     * Устанавливает имя сотрудника.
+     * Имя будет установлено только если переданная строка не null и не пустая.
+     *
+     * @param first_name имя сотрудника
+     * @throws IllegalArgumentException если имя null или пустое
+     */
+    public void setFirst_name(String first_name) {
+        if (first_name != null && !first_name.isBlank()) {
+            this.first_name = first_name;
+        } else {
+            throw new IllegalArgumentException("Поле не может быть пустым");
+        }
+    }
+
+    /**
+     * Возвращает фамилию сотрудника.
+     *
+     * @return фамилия сотрудника
+     */
+    public String getLast_name() {
+        return last_name;
+    }
+
+    /**
+     * Устанавливает фамилию сотрудника.
+     * Фамилия будет установлена только если переданная строка не null и не пустая.
+     *
+     * @param last_name фамилия сотрудника
+     * @throws IllegalArgumentException если поле null или пустое
+     */
+    public void setLast_name(String last_name) {
+        if (last_name != null && !last_name.isBlank()) {
+            this.last_name = last_name;
+        } else {
+            throw new IllegalArgumentException("Поле не может быть пустым");
+        }
+    }
+
+    /**
+     * Возвращает отчество сотрудника.
+     *
+     * @return отчество сотрудника
+     */
+    public String getMiddle_name() {
+        return middle_name;
+    }
+
+    /**
+     * Устанавливает отчество сотрудника.
+     * Отчество будет установлено только если переданная строка не null и не пустая.
+     *
+     * @param middle_name отчество сотрудника
+     * @throws IllegalArgumentException если поле null или пустое
+     */
+    public void setMiddle_name(String middle_name) {
+        if (last_name != null && !middle_name.isBlank()) {
+            this.middle_name = middle_name;
+        } else {
+            throw new IllegalArgumentException("Поле не может быть пустым");
+        }
+    }
+
+    /**
+     * Возвращает отдел сотрудника.
+     *
+     * @return отдел сотрудника
+     */
+    public int getDepartment() {
+        return department;
+    }
+
+    /**
+     * Устанавливает номер отдела сотрудника.
+     * Отдел будет установлен только если значение больше 0.
+     *
+     * @param department отдел сотрудника
+     * @throws IllegalArgumentException если значение меньше или равно 0
+     */
+    public void setDepartment(int department) {
+        if (department > 0) {
+            this.department = department;
+        } else {
+            throw new IllegalArgumentException("Значение должно быть больше 0");
+        }
+    }
+
+    /**
+     * Возвращает зарплату сотрудника.
+     *
+     * @return зарплата сотрудника
+     */
+    public float getSalary() {
+        return salary;
+    }
+
+    /**
+     * Устанавливает зарплату сотрудника.
+     * Зарплата будет установлена только если значение больше 0.
+     *
+     * @param salary зарплата сотрудника
+     * @throws IllegalArgumentException если значение меньше или равно 0
+     */
+    public void setSalary(float salary) {
+        if (salary > 0) {
+            this.salary = salary;
+        } else {
+            throw new IllegalArgumentException("Значение должно быть больше 0");
+        }
+    }
+
+    /**
+     * Возвращает уникальный id сотрудника.
+     *
+     * @return id сотрудника
+     */
+    public static int getId() {
+        return id;
+    }
+}
